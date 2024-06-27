fcf4b73ac3d239ff6f00bd083bdd191b5bddd62e
Raph43lD3V <raphael-lacerda75@hotmail.com>
2024-06-27 20:53:23 -0300

    adicionando primeiro commit, com o primeiro desafio

---
+++ app.py
@@ -0,0 +1,11 @@
+print("""
+█▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
+▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█""")
+
+print('1. Cadastrar Restaurante')
+print('2. listar Restaurante')
+print('3. ativar Restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma das opções: ')
+print(f'você escolheu a opção, {opcao_escolhida}')

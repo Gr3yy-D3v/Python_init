1ded361bef864c33d426a16f2290fb85ecb4661b
Raph43lD3V <raphael-lacerda75@hotmail.com>
2024-07-01 18:53:52 -0300

    adicionando desafio para verificar se o número é par ou impar

--- app_chalenges.py
+++ app_chalenges.py
@@ -1,3 +1,15 @@
-pi = 3.14159
-pi_arredondado = round(pi, 2)
-print(f'O valor arredondado de pi é {pi_arredondado}')
+# Função para verificar se um número é par ou ímpar
+def verificar_paridade(numero):
+    if numero % 2 == 0:
+        return "par"
+    else:
+        return "ímpar"
+
+# Solicita ao usuário que insira um número
+numero = int(input("Digite um número: "))
+
+# Verifica se o número é par ou ímpar
+resultado = verificar_paridade(numero)
+
+# Exibe o resultado
+print(f"O número {numero} é {resultado}.")

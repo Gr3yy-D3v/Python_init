a11d8d12c9ae3d95294cfbc38a739f4bbdb5e749
Raph43lD3V <raphael-lacerda75@hotmail.com>
2024-07-01 12:35:49 -0300

    adicionando if, elif e else

--- app.py
+++ app.py
@@ -7,5 +7,14 @@ print('2. listar Restaurante')
 print('3. ativar Restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma das opções: ')
-print(f'você escolheu a opção, {opcao_escolhida}')
+opcao_escolhida = int(input('Escolha uma das opções: '))
+
+
+if opcao_escolhida == 1 :
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2 :
+    print('Listar restaurantes')
+elif opcao_escolhida == 3 :
+    print('ativar restaurantes')
+else:
+    print('Encerrando o programa')

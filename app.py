dc8bbce140e00d0d19f34caa3ea1d8d917448eda
Raph43lD3V <raphael-lacerda75@hotmail.com>
2024-07-01 12:59:51 -0300

    adicionando novas funções com Def

--- app.py
+++ app.py
@@ -1,20 +1,44 @@
-print("""
+import os
+
+def exibir_nome_do_programa():
+    print("""
 █▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
 ▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█""")
 
-print('1. Cadastrar Restaurante')
-print('2. listar Restaurante')
-print('3. ativar Restaurante')
-print('4. Sair\n')
+def exibir_opcoes():
+    print('1. Cadastrar Restaurante')
+    print('2. listar Restaurante')
+    print('3. ativar Restaurante')
+    print('4. Sair\n')
 
-opcao_escolhida = int(input('Escolha uma das opções: '))
+
+def finalizar_app():
+    os.system('cls')
+    #os.system('clear') no mac
+    print('Finalizando o app\n')
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma das opções: '))
+    #opcao_escolhida = int(opcao_escolhida)
+    """
+    Def = Definir uma função para um fim. Utilizado para organizar o código.
+    """
 
 
-if opcao_escolhida == 1 :
-    print('Cadastrar restaurante')
-elif opcao_escolhida == 2 :
-    print('Listar restaurantes')
-elif opcao_escolhida == 3 :
-    print('ativar restaurantes')
-else:
-    print('Encerrando o programa')
+    if opcao_escolhida == 1 :
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2 :
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3 :
+        print('ativar restaurantes')
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+
+if __name__ == '__main__':
+    main()

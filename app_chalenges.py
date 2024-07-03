2b76deac75988e803ebb67a38ef1f42634c63607
Raph43lD3V <raphael-lacerda75@hotmail.com>
2024-07-03 18:07:19 -0300

    adicionando desafio para verificar usuário

--- app_chalenges.py
+++ app_chalenges.py
@@ -1,11 +1,13 @@
-idade = int(input('coloque sua idade: '))
+# Define os valores esperados para o nome de usuário e a senha
+usuario_esperado = "usuario123"
+senha_esperada = "senha123"
 
-if idade < 0:
-    print('idade invalida, coloque um número positivo')
-if idade <= 12:
-    print('você é uma criança')
-elif idade <= 18:
-    print('você é um adolescente')
+# Solicita ao usuário que insira o nome de usuário e a senha
+usuario_fornecido = input("Digite o nome de usuário: ")
+senha_fornecida = input("Digite a senha: ")
+
+# Verifica se o nome de usuário e a senha correspondem aos valores esperados
+if usuario_fornecido == usuario_esperado and senha_fornecida == senha_esperada:
+    print("Acesso concedido.")
 else:
-    print('você é um adulto')
-
+    print("Nome de usuário ou senha incorretos. Acesso negado.")

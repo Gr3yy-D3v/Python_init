04cf01d90de7a8c74d036af5bf03f438f9bb5250
Raph43lD3V <raphael-lacerda75@hotmail.com>
2024-07-04 18:45:18 -0300

    adicionando desafio de loop para fazer uma oderm decrescente de 1 a 10

--- app_chalenges.py
+++ app_chalenges.py
@@ -1,12 +1,3 @@
-# Inicializa a variável para armazenar a soma
-soma = 0
-
-# Usa um loop for para percorrer os números de 1 a 10
-for numero in range(1, 11):
-    # Verifica se o número é ímpar
-    if numero % 2 != 0:
-        # Adiciona o número ímpar à soma
-        soma += numero
-
-# Exibe o resultado
-print("A soma dos números ímpares de 1 a 10 é:", soma)
+# Usa um loop for para percorrer os números de 10 a 1
+for numero in range(10, 0, -1):
+    print(numero)

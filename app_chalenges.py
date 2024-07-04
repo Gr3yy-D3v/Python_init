cd978db76362ff4f8ff13c56e824a66da19c8a7b
Raph43lD3V <raphael-lacerda75@hotmail.com>
2024-07-04 18:43:36 -0300

    adicionando desafio de loop de 1 a 10, agora somando os números impares

--- app_chalenges.py
+++ app_chalenges.py
@@ -1,5 +1,12 @@
-list_number = [1,2,3,4,5,6,7,8,9,10]
-list_name = ["Raphael","Miguel","Samuel"]
+# Inicializa a variável para armazenar a soma
+soma = 0
 
-for number in list_number:
-    print(number)
+# Usa um loop for para percorrer os números de 1 a 10
+for numero in range(1, 11):
+    # Verifica se o número é ímpar
+    if numero % 2 != 0:
+        # Adiciona o número ímpar à soma
+        soma += numero
+
+# Exibe o resultado
+print("A soma dos números ímpares de 1 a 10 é:", soma)

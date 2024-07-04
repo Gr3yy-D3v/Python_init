94fc70e026de2eaac336948ff0e645f9df0ac23c
Raph43lD3V <raphael-lacerda75@hotmail.com>
2024-07-04 18:35:28 -0300

    Melhorando o código com mais funções e deixando-o mais limpo

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+#Importando uma Biblioteca de fora do Python
 import os
+#lista em Python e definida por: []
+restaurantes = ['Pizza', 'Sushi']
 
+#Def é como se define uma variável em Python
 def exibir_nome_do_programa():
+    #""" para colocar um comentario de varias linhas
     print("""
 █▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
 ▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█""")
@@ -11,30 +16,60 @@ def exibir_opcoes():
     print('3. ativar Restaurante')
     print('4. Sair\n')
 
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print()
 
 def finalizar_app():
-    os.system('cls')
-    #os.system('clear') no mac
-    print('Finalizando o app\n')
+    exibir_subtitulo('Finalizando programa')
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu principal')
+    main()
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo('Listando restaurantes')
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+    voltar_ao_menu_principal()
+
+
+def opcao_invalida():
+    print('Opção inválida!\n')
+    voltar_ao_menu_principal()
 
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma das opções: '))
-    #opcao_escolhida = int(opcao_escolhida)
-    """
-    Def = Definir uma função para um fim. Utilizado para organizar o código.
-    """
+    # Como o próprio nome diz, vai tentar uma logica se não funcionar fará outra coisa
+    try:
+        #opcao_escolhida = int(opcao_escolhida)
+        opcao_escolhida = int(input('Escolha uma das opções: '))
+        """
+        Def = Definir uma função para um fim. Utilizado para organizar o código.
+        """
+        if opcao_escolhida == 1 :
+            cadastrar_novo_restaurante()
 
-
-    if opcao_escolhida == 1 :
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2 :
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3 :
-        print('ativar restaurantes')
-    else:
-        finalizar_app()
+        elif opcao_escolhida == 2 :
+            listar_restaurantes()
+        elif opcao_escolhida == 3 :
+            print('ativar restaurantes')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()

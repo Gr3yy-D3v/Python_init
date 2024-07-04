c76b462280b821cbb874a125cd0efe7e82696c7c
Raph43lD3V <raphael-lacerda75@hotmail.com>
2024-07-04 18:42:05 -0300

    adicionando desafio de loop de 1 a 10 no chalenges

--- app_chalenges.py
+++ app_chalenges.py
@@ -1,13 +1,5 @@
-coordenada_x = int(input("me diga um numero onde está X "))
-coordenada_Y = int(input("me diga um numero onde está Y "))
+list_number = [1,2,3,4,5,6,7,8,9,10]
+list_name = ["Raphael","Miguel","Samuel"]
 
-if coordenada_x and coordenada_Y > 0:
-    print("Você está no primeiro quadrante")
-elif coordenada_x < 0 and coordenada_Y > 0:
-    print("Você está no segundo quadrante")
-elif coordenada_x and coordenada_Y < 0:
-    print("Você está no terceiro quadrante")
-elif coordenada_x > 0 and coordenada_Y < 0:
-    print("Você está no quarto quadrante")
-else:
-    print("Você está no ponto de origem")
+for number in list_number:
+    print(number)

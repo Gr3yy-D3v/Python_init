ecdcc6aada41dcdb3bd007e5dab3d53e458f77c9
Raph43lD3V <raphael-lacerda75@hotmail.com>
2024-07-04 18:47:35 -0300

    adicionando desafio de loop para tabuada

--- app_chalenges.py
+++ app_chalenges.py
@@ -1,3 +1,7 @@
-# Usa um loop for para percorrer os números de 10 a 1
-for numero in range(10, 0, -1):
-    print(numero)
+# Solicita ao usuário que insira um número
+numero = int(input("Digite um número para ver sua tabuada: "))
+
+# Usa um loop for para percorrer os números de 1 a 10
+for o in range(1, 11):
+    resultado = numero * o
+    print(f"{numero} x {o} = {resultado}")

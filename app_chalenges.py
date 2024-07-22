5bbb04b023c85466d51142fbea5bef10171b3f9f
Gr3yy-D3v <raphael-lacerda75@hotmail.com>
2024-07-22 12:56:39 -0300

    adicionando o começo do desafio

--- app_chalenges.py
+++ app_chalenges.py
@@ -1,21 +1,6 @@
-# Cria uma lista de números (pode estar vazia para testar a exceção)
-numeros = [8,20,45,63,250,20]
-
-try:
-    # Calcula a soma dos elementos da lista
-    soma = sum(numeros)
-    
-    # Calcula a quantidade de elementos na lista
-    quantidade = len(numeros)
-    
-    # Tenta calcular a média dos elementos da lista
-    media = soma / quantidade
-    
-    # Exibe o resultado da média
-    print("A média dos valores na lista é:", media)
-except ZeroDivisionError as e:
-    # Captura e lida com a exceção de divisão por zero
-    print("Erro: não é possível calcular a média de uma lista vazia.")
-except Exception as e:
-    # Captura e lida com qualquer outra exceção
-    print("Ocorreu um erro inesperado:", e)
+#Dicionario sobre pessoa Julia
+people = {
+    'Nome': 'Julia',
+    'Idade': 18,
+    'Cidade': 'São Paulo'
+}
